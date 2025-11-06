58c4bc47d5fc4368850df2a4ffa19016b53e002f
Carlos Sac <carlos.sac@outlook.com>
2025-11-05 21:40:49 -0500

    Initial Commit

---
+++ RoomEase/RoomEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomEaseApp.swift
+//  RoomEase
+//
+//  Created by Carlos Sac on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RoomEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
