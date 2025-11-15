bed02023ecce04b7fd7503a207ac8b4b6e5e7131
Yaroslav Petrunin <iaroslavpetrunin@gmail.com>
2025-11-15 18:11:48 -0500

    Created Chore Detail View as well as the AddChore view

--- RoomEase/Chore.swift
+++ RoomEase/Chore.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 
-struct Chore: Identifiable, Codable {
+struct Chore: Hashable,Identifiable, Codable {
     let id: String
     var name: String
     var isCompleted: Bool
+    var days: [String]
+
     
-    init(id: String = UUID().uuidString, name: String, isCompleted: Bool = false) {
+    init(id: String = UUID().uuidString, name: String, isCompleted: Bool = false, days: [String] = []) {
         self.id = id
         self.name = name
         self.isCompleted = isCompleted
+        self.days = days
     }
 }
