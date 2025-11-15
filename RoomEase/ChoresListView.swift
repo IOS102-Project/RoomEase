9ff0fd86be47ff0bbdb453429f168611282ea8b0
Carlos Sac <carlos.sac@outlook.com>
2025-11-15 16:36:28 -0500

    Add NavegationTabView

---
+++ RoomEase/ChoresListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChoresListView.swift
+//  RoomEase
+//
+//  Created by Carlos Sac on 11/15/25.
+//
+
+import SwiftUI
+
+struct ChoresListView: View {
+    var body: some View {
+        Text("Chores List View")
+    }
+}
+
+#Preview {
+    ChoresListView()
+}
---
+++ RoomEase/ShoppingListView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingListView.swift
+//  RoomEase
+//
+//  Created by Carlos Sac on 11/15/25.
+//
+
+import SwiftUI
+
+struct ShoppingListView: View {
+    var body: some View {
+        Text("Shopping List View")
+    }
+}
+
+#Preview {
+    ShoppingListView()
+}
